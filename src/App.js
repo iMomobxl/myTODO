313bdc6446b8e2dc1533c3af2cbdb706cfd9ce95
iMomobxl <mohamed.alibacha@gmail.com>
2025-02-01 18:01:05 +0100

    create the structure of the home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO app</h1>
+      <div className="todo-wrapper">
+
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the task description?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">Add</button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button>TODO</button>
+          <button>Completed</button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
